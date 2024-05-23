5349075709029952cce4229291d901a865bcfd6a
abdo-elshaikh <abdo_mhmd1@hotmail.com>
2024-05-23 15:06:05 +0300

    update fifo cache

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -23,11 +23,13 @@ class FIFOCache(BaseCaching):
            `item` value for the key `key`
         '''
 
-        if key is not None or item is not None:
+        if key is not None and item is not None:
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
                 first_key, _ = self.cache_data.popitem(last=False)
-                print(f"DISCARD: {first_key}")
+                print(f"DISCARD: {first_key}")                
             self.cache_data[key] = item
+        else:
+            return
 
     def get(self, key):
         '''return the value in `self.cache_data` linked to `key`
